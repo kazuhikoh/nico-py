760584f17c387dc4bdca422d364c2e273d2f8409
kazuhikoh <kazuhiko.hrmt@gmail.com>
2021-04-03 03:49:11 +0900

    ✨ list <channel>

--- nico/__init__.py
+++ nico/__init__.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import sys
 import click
 
 from . import command
@@ -8,6 +9,12 @@ from . import command
 @click.group()
 def cli():
     pass
+
+@cli.command()
+@click.argument('channel')
+def list(channel):
+    ok = command.list(channel)
+    sys.exit( 0 if ok else 1 )
 
 @cli.command()
 @click.argument('input') 
--- nico/command.py
+++ nico/command.py
@@ -1,5 +1,26 @@
 from . import config
+from . import httpclient
 from . import browser
+
+def list(channelId):
+    cfg = config.load()
+
+    opener = httpclient.createClient()
+    httpclient.login(opener, cfg['username'], cfg['password'])
+
+    pageMax = httpclient.getVideoPageMax(opener, channelId)
+
+    if (pageMax == -1):
+        return False
+
+    for i in range(pageMax):
+        items = httpclient.getVideoItems(opener, channelId, i+1)
+        for it in items:
+            id = it['id']
+            title = it['title']
+            print(f'{id} "{title}"')
+
+    return True
 
 def download(url, outpath):
     cfg = config.load()
